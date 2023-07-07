7982cd54dfc35059fad70c3d526f4a88f006d585
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-07 17:27:25 -0600

    the test for search is created

--- tests/test_define_savor.py
+++ tests/test_define_savor.py
@@ -1,5 +1,5 @@
 import unittest
-from ceval.defines_saver import check_type, MacroSaver
+from ceval.defines_saver import check_type, MacroSaver, Macro
 
 
 class test(unittest.TestCase):
@@ -30,7 +30,20 @@ class test(unittest.TestCase):
 
         self.assertEqual(a, True)
 
+    def test_search_class_MacroSaver(self):
 
+        obj = MacroSaver()
 
+        macro_1 = Macro("AH", "5")
+        macro_2 = Macro("AI", "5")
+        macro_3 = Macro("AL", "5")
 
+        obj += macro_1
+        obj += macro_2
+        obj += macro_3
 
+        result = obj.search("AH")
+        result_1 = obj.search("hh")
+
+        result_all = [result, result_1]
+        self.assertEqual(result_all, [("AH", 0), None])
